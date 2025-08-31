414d0d003e83382fa719e59bec029705971cc633
KKartikay-27 <kumarkartikay005@gmail.com>
2025-09-01 01:10:35 +0530

    Completed ex01

--- singleton-config/src/com/example/config/AppSettings.java
+++ singleton-config/src/com/example/config/AppSettings.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
 
+import javax.management.RuntimeErrorException;
+
 /**
  * FAULTY "Singleton": public constructor, getInstance() returns a NEW instance each time,
  * not thread-safe, reload allowed anytime, mutable global state, reflection+serialization-friendly.
@@ -12,10 +14,27 @@ import java.util.Properties;
 public class AppSettings implements Serializable {
     private final Properties props = new Properties();
 
-    public AppSettings() { } // should not be public for true singleton
+    private static volatile AppSettings instance;
+
+    AppSettings() {
+        if(instance != null) throw new RuntimeException("Use getInstance()");
+     } // should not be public for true singleton
 
     public static AppSettings getInstance() {
-        return new AppSettings(); // returns a fresh instance (bug)
+        if(instance == null){
+            synchronized (AppSettings.class){
+                if(instance == null){
+                    instance = new AppSettings();
+                }
+            }
+        }
+        return instance;
+        
+        // return new AppSettings(); // returns a fresh instance (bug)
+    }
+
+    private Object readResolve(){
+        return getInstance();
     }
 
     public void loadFromFile(Path file) {
